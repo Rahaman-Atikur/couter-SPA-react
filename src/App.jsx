334f3ece18df226acf9672db92eeeab2b8067c30
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-09 21:33:33 +0100

    Button and a paragraph added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+// const [counter , setCounter] = useState(0);
+
 
 const App = () => {
+  const [counter, setCounter] = useState([0]);
   return (
-    <div>
-      <h1>This is flawless</h1>
+    <div className='m-10 flex justify-center items-center gap-5'>
+      <button className='btn-primary'>Click ME</button>
+      <p>Total Clicked Time:{counter}</p>
     </div>
   );
 };

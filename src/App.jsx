dc76ec1e2c3de0e0a63f63b81428f978ba333a6c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-10 08:09:48 +0100

    Event handler Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-  const [counter, setCounter] = useState([0]);
+  const [counter, setCounter] = useState(0);
+  const handleClick = () => {
+    setCounter(counter + 1);
+  }
+  const handleEventHandler = (e) => {
+    e.preventDefault()
+    console.log(e.target.name.value);
+  }
   return (
-    <div className='m-10 flex justify-center items-center gap-5'>
-      <button className='btn-primary'>Click ME</button>
-      <p>Total Clicked Time:{counter}</p>
+    <div className='m-10 flex flex-col justify-center items-center gap-5'>
+      <form action="">
+        <input className='input input-primary' placeholder='Your Name' name='name' type="text" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
